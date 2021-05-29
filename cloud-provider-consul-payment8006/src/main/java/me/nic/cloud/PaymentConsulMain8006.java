d535a101055c35b1fdddac446794d0f4263b5586
nicahead <nicahead@gmail.com>
2021-05-29 16:53:34 +0800

    使用consul作为注册中心

---
+++ cloud-provider-consul-payment8006/src/main/java/me/nic/cloud/PaymentConsulMain8006.java
@@ -0,0 +1,11 @@
+package me.nic.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentConsulMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentConsulMain8006.class);
+    }
+}
